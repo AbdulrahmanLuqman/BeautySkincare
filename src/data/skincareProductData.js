e8c1bdc0d405cb571744b13b788fef0dedef5570
AbdulrahmanLuqman <fal528@dervac.com>
2024-07-06 10:53:40 +0100

    set up the project

---
+++ src/data/skincareProductData.js
@@ -0,0 +1,27 @@
+[
+    {
+        image: "../assets/abbottCream.png",
+        name: "Abbott Rx Hydroquinone Cream USP",
+        price: "# 10,000"
+    },
+    {
+        image: "../assets/abbottCream.png",
+        name: "Abbott Rx Hydroquinone Cream USP",
+        price: "# 10,000"
+    },
+    {
+        image: "../assets/abbottCream.png",
+        name: "Abbott Rx Hydroquinone Cream USP",
+        price: "# 10,000"
+    },
+    {
+        image: "../assets/abbottCream.png",
+        name: "Abbott Rx Hydroquinone Cream USP",
+        price: "# 10,000"
+    },
+    {
+        image: "../assets/abbottCream.png",
+        name: "Abbott Rx Hydroquinone Cream USP",
+        price: "# 10,000"
+    }
+]
