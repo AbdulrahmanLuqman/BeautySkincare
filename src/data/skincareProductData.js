09bc402664c606a8f78a003c394b1b0a811ff7f1
AbdulrahmanLuqman <fal528@dervac.com>
2024-07-07 13:06:09 +0100

    added products

--- src/data/skincareProductData.js
+++ src/data/skincareProductData.js
@@ -8,26 +8,68 @@ export const productsData = [
     },
     {
         index: 1,
-        image: "../assets/productsImage/abbottCream.png",
-        name: "Abbott Rx Hydroquinone Cream USP",
+        image: "src/assets/productsImage/joesonMilk.png",
+        name: "Joeson Glow Rice Milk 120ml",
         price: "# 10,000"
     },
     {
         index: 2,
-        image: "../assets/productsImage/abbottCream.png",
-        name: "Abbott Rx Hydroquinone Cream USP",
+        image: "src/assets/productsImage/vaselineGlow.png",
+        name: "Vaseline Smooth Glow 120ml",
         price: "# 10,000"
     },
     {
         index: 3,
-        image: "../assets/productsImage/abbottCream.png",
-        name: "Abbott Rx Hydroquinone Cream USP",
+        image: "src/assets/productsImage/hydroquinonecream.png",
+        name: "Hydroquinone Cream USP 30g",
         price: "# 10,000"
     },
     {
         index: 4,
-        image: "../assets/productsImage/abbottCream.png",
+        image: "src/assets/productsImage/abbottCream.png",
         name: "Abbott Rx Hydroquinone Cream USP",
+        price: "# 10,000"
+    },
+    {
+        index: 5,
+        image: "src/assets/productsImage/joesonMilk.png",
+        name: "Joeson Glow Rice Milk 120ml",
+        price: "# 10,000"
+    },
+    {
+        index: 6,
+        image: "src/assets/productsImage/vaselineGlow.png",
+        name: "Vaseline Smooth Glow 120ml",
+        price: "# 10,000"
+    },
+    {
+        index: 7,
+        image: "src/assets/productsImage/hydroquinonecream.png",
+        name: "Hydroquinone Cream USP 30g",
+        price: "# 10,000"
+    },
+    {
+        index: 8,
+        image: "src/assets/productsImage/abbottCream.png",
+        name: "Abbott Rx Hydroquinone Cream USP",
+        price: "# 10,000"
+    },
+    {
+        index: 9,
+        image: "src/assets/productsImage/joesonMilk.png",
+        name: "Joeson Glow Rice Milk 120ml",
+        price: "# 10,000"
+    },
+    {
+        index: 10,
+        image: "src/assets/productsImage/vaselineGlow.png",
+        name: "Vaseline Smooth Glow 120ml",
+        price: "# 10,000"
+    },
+    {
+        index: 11,
+        image: "src/assets/productsImage/hydroquinonecream.png",
+        name: "Hydroquinone Cream USP 30g",
         price: "# 10,000"
     }
 ];
